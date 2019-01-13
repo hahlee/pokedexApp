5bc3566af69e5dd958331aaea3da2cd27931b693
hahlee <hahlee93@gmail.com>
2019-01-12 17:09:33 -0700

    Updates

---
+++ precache-manifest.786855ace2431294240447b23aacaa6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a398990b1d7b6c11135bd5823555f99",
+    "url": "/pokedexApp/static/media/sprites.6a398990.png"
+  },
+  {
+    "revision": "e51aa69ded6a719c024e",
+    "url": "/pokedexApp/static/js/runtime~main.e51aa69d.js"
+  },
+  {
+    "revision": "bff94a12f62d83016915",
+    "url": "/pokedexApp/static/js/main.bff94a12.chunk.js"
+  },
+  {
+    "revision": "848793fa030cb3342b3b",
+    "url": "/pokedexApp/static/js/1.848793fa.chunk.js"
+  },
+  {
+    "revision": "bff94a12f62d83016915",
+    "url": "/pokedexApp/static/css/main.575a0a72.chunk.css"
+  },
+  {
+    "revision": "4bb12d0e07524b882fe1c6a24904e466",
+    "url": "/pokedexApp/index.html"
+  }
+];
